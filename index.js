0c31610635f4976ef0d8ec6bf6a435bf97cbcbf7
Daniel <skap.tk@gmail.com>
2022-05-14 00:46:50 -0500

    Added frontend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan")
 const app = express();
 
+app.use(express.static("build"))
+
 morgan.token('post-data', (request, response) => {
     if (request.method === 'POST'){
         return JSON.stringify(request.body)
